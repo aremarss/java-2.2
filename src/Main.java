180bcbb97f42b1a5d09ec521c515512626c9df6d
aremarss <aremars@mail.ru>
2021-10-06 20:36:27 +0300

    Edit Main

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ public class Main {
         int cash = 100; // Количество денег у клиента на счету.
         int cashAdd = 1100; // Количество денег, которые хочет пополнить клиент.
         int bonusAmount; // Сумма бонусов.
-        int totalCash; // Итоговое количество денег не счете.
+        int cashTotal; // Итоговое количество денег не счете.
 
         if (cashAdd >= 1000) {
             bonusAmount = cashAdd / 100; // Можно менять количество рублей, с которых будет начисляться один бонус.
-            totalCash = cash + cashAdd + bonusAmount;
+            cashTotal = cash + cashAdd + bonusAmount;
 
         } else {
             bonusAmount = 0;
-            totalCash = cash + cashAdd;
+            cashTotal = cash + cashAdd;
         }
-        System.out.println("Количество денег на текущем счете клиента: " + totalCash + " руб.");
-        System.out.println("Количество бонусных рублей: " + bonusAmount + " руб.");// Бонусы не показываются.
+        System.out.println("Количество денег на текущем счете клиента: " + cashTotal + " руб.");
+        System.out.println("Количество бонусных рублей: " + bonusAmount + " руб.");
     }
 }
